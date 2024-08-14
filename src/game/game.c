2a81202c9a7bca1219733d01f699643134980183
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2024-08-14 22:35:12 +0100

    camera follows a player capsule

--- src/game/game.c
+++ src/game/game.c
@@ -13,7 +13,11 @@
 
 // Example game state
 struct GameState {
-    Camera3D camera;
+    struct Player {
+        Camera3D player_cam;
+        Vector3 player_pos;
+        float player_speed;
+    } player;
     int num;
 };
 
@@ -23,13 +27,19 @@ void* initialise() {
     DisableCursor();
 
     struct GameState *state = malloc(sizeof(struct GameState));
+    state->player.player_pos = (Vector3){ 0.0f, 0.0f, 0.0f };   
+    state->player.player_speed = 3.0f;
+
     state->num = 1;
-    state->camera.position = (Vector3){ 0.0f, 20.0f, 0.1f }; 
-    state->camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      
-    state->camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          
-    state->camera.fovy = 45.0f;                                
-    state->camera.projection = CAMERA_PERSPECTIVE;            
-     
+    state->player.player_cam.position = (Vector3){ 
+        state->player.player_pos.x, 
+        state->player.player_pos.y + 20.0f, 
+        state->player.player_pos.z + 0.1f }; 
+
+    state->player.player_cam.target = (Vector3){ 0.0f, 0.0f, 0.0f };      
+    state->player.player_cam.up = (Vector3){ 0.0f, 1.0f, 0.0f };          
+    state->player.player_cam.fovy = 45.0f;                                
+    state->player.player_cam.projection = CAMERA_PERSPECTIVE;    
 
     // Return void ptr to state
     return (void*)state;
@@ -41,11 +51,25 @@ void update(double dt, void* state) {
     fprintf(stdout, "update: %d\n", game_state->num);
     
     game_state->num++;
-    UpdateCamera(&game_state->camera, CAMERA_PERSPECTIVE);
-    
-    if (IsKeyPressed('Z')){
-        game_state->camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+
+    if (IsKeyDown('W')) {
+        game_state->player.player_pos.z -= game_state->player.player_speed * dt;
+    } 
+    if (IsKeyDown('S')) {
+        game_state->player.player_pos.z += game_state->player.player_speed * dt;
+    }  
+    if (IsKeyDown('A')) {
+        game_state->player.player_pos.x -= game_state->player.player_speed * dt;
+    }  
+    if (IsKeyDown('D')) {
+        game_state->player.player_pos.x += game_state->player.player_speed * dt;
     }
+    
+    game_state->player.player_cam.position = (Vector3) { 
+        game_state->player.player_pos.x, 
+        game_state->player.player_pos.y + 20.0f, 
+        game_state->player.player_pos.z + 0.1f }; 
+    game_state->player.player_cam.target = game_state->player.player_pos;
 }
 
 void render(double dt, const void* state) {
@@ -56,23 +80,18 @@ void render(double dt, const void* state) {
 
     ClearBackground(RAYWHITE);
 
-    BeginMode3D(game_state->camera);
+    BeginMode3D(game_state->player.player_cam);
 
-    DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-    DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+    DrawCylinder(game_state->player.player_pos, 1.0f, 1.0f, 3.0f, 100, RED);
 
     DrawGrid(10, 1.0f);
 
     EndMode3D();
 
-    DrawRectangle( 10, 10, 320, 113, Fade(SKYBLUE, 0.5f));
-    DrawRectangleLines( 10, 10, 320, 113, BLUE);
+    DrawRectangle( 10, 10, 100, 33, Fade(SKYBLUE, 0.5f));
+    DrawRectangleLines( 10, 10, 100, 33, BLUE);
 
-    DrawText("Free camera default controls:", 20, 20, 10, BLACK);
-    DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
-    DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
-    DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
-    DrawText(TextFormat("FPS: %.2f", 1.0f/dt), 40, 100, 10, DARKGRAY);
+    DrawText(TextFormat("FPS: %.2f", 1.0f/dt), 40, 20, 10, DARKGRAY);
 
     EndDrawing();
     fprintf(stdout, "render: %d\n", ((struct GameState*)state)->num);
